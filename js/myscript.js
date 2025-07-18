56e4f5a2b43697d00a5da21d880f49854417913d
Grocat <ajithkumarselvamak2000@gmail.com>
2025-07-18 17:30:56 +0530

    Update myscript.js

--- js/myscript.js
+++ js/myscript.js
@@ -1,49 +1,91 @@
+$(document).ready(function () {
+  $(".home-car").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    nav: false,
+    dots: true
+  });
+});
+$(document).ready(function () {
+  $('.owl-carousel.word-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    smartSpeed: 600,
+    navText: ['<span class="owl-prev-arrow"><i class="fa-solid fa-arrow-left"></i></span>', '<span class="owl-next-arrow"><i class="fa-solid fa-arrow-right"></i></span>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      }
+    }
+  });
+});
 
-  $(document).ready(function(){
-    $(".home-car").owlCarousel({
+$(document).ready(function () {
+  $('.art_meet_slider').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 600,
+    navText: ['<span class="owl-prev-arrow"><i class="fa-solid fa-arrow-left"></i></span>', '<span class="owl-next-arrow"><i class="fa-solid fa-arrow-right"></i></span>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1024: {
+        items: 2
+      }
+    }
+  });
+});
+
+  $(document).ready(function() {
+    $(".image_slider").owlCarousel({
       items: 1,
       loop: true,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: true,
-      nav: false,
-      dots: true
-    });
-  });
-$(document).ready(function(){
-    $('.owl-carousel.word-carousel').owlCarousel({
-      loop: true,
-      margin: 30,
+      margin: 10,
       nav: true,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: true,
-      smartSpeed: 600,
-      navText: ['<span class="owl-prev-arrow"><i class="fa-solid fa-arrow-left"></i></span>', '<span class="owl-next-arrow"><i class="fa-solid fa-arrow-right"></i></span>'],
-      responsive:{
-        0:{ items:1 },
-        768:{ items:1 },
-        1024:{ items:1 }
-      }
     });
-  });
 
-  $(document).ready(function(){
-    $('.art_meet_slider').owlCarousel({
+    $(".video_slider").owlCarousel({
+      items: 1,
       loop: true,
-      margin: 30,
+      margin: 10,
       nav: true,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 4000,
-      smartSpeed: 600,
-      navText: ['<span class="owl-prev-arrow"><i class="fa-solid fa-arrow-left"></i></span>', '<span class="owl-next-arrow"><i class="fa-solid fa-arrow-right"></i></span>'],
-      responsive:{
-        0:{ items:1 },
-        768:{ items:2 },
-        1024:{ items:2 }
-      }
+    });
+
+    $('#imageTab').click(function(e) {
+      e.preventDefault();
+      $('.image_slider').show();
+      $('.video_slider').hide();
+      $('#imageTab').addClass('active');
+      $('#videoTab').removeClass('active');
+    });
+
+    $('#videoTab').click(function(e) {
+      e.preventDefault();
+      $('.video_slider').show();
+      $('.image_slider').hide();
+      $('#videoTab').addClass('active');
+      $('#imageTab').removeClass('active');
     });
   });
-
